19771d99644fe3c273fac2c7e284e4c2af1b420e
Sathar Suhel <satharsuhel.learner@gmail.com>
2025-01-03 20:10:50 +0530

    Expense Shell

--- backend.sh
+++ backend.sh
@@ -43,8 +43,14 @@ VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing NodeJS 20"
 
-useradd expense &>>$LOG_FILE_NAME
-VALIDATE $? "Adding expense"
+id expense &>>$LOG_FILE_NAME
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOG_FILE_NAME
+    VALIDATE $? "Adding expense"
+else
+    echo -e "expense user already exits ... $Y SKIPPING $N"
+fi        
 
 mkdir /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating app directory"
