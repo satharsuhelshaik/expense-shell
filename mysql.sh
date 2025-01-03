ade429d38e0399f1c97b628a89c5680a54d524f6
Sathar Suhel <satharsuhel.learner@gmail.com>
2025-01-03 18:17:58 +0530

    Expense Shell

--- mysql.sh
+++ mysql.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER="var/log/expense-logs"
+LOG_FOLDER="/var/log/expense-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$"(date +%Y-%m-%d-%H-%M-%S)"
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
